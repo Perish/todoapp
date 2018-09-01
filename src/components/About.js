944eec8302ce1e7cfd880e5e29655c85f0a1f48f
xc <xuchong@xcdeMacBook-Pro.local>
2018-09-01 10:22:12 +0800

    todoapp

---
+++ src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Rainbow from '../hoc/Rainbow'
+
+const About = () => {
+  return (
+    <div className="container">
+      <h4 className="center">About</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam nostrum eaque sequi, ad culpa similique sint adipisci pariatur magnam quasi error voluptate mollitia nesciunt ipsum dolores ex neque, aut praesentium.</p>
+    </div>
+  )
+}
+
+export default Rainbow(About)
---
+++ src/components/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className="container">
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam nostrum eaque sequi, ad culpa similique sint adipisci pariatur magnam quasi error voluptate mollitia nesciunt ipsum dolores ex neque, aut praesentium.</p>
+    </div>
+  )
+}
+
+export default Contact
